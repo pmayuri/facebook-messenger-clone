95b3746ce185eb4725149db57ecc0fec5c8f0c05
mayuri <pmayuri124@gmail.com>
2020-10-29 20:17:05 +0530

    message bar completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+   const [input,setInput] = useState('');
+    const [messages,setMessages] = useState(['hello','hi']);
+
+console.log(input);
+console.log(messages);
+
+const sendMessage = (event) => {
+    event.preventDefault();
+  // all the logic to send a message goes here
+  setMessages([...messages,input]);
+  setInput('');
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <form>
+       <input value={input} onChange={event => setInput(event.target.value)} />
+       <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
     </div>
+
+  
   );
 }
 
