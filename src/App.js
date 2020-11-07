9cac3dda10ac8ced98c66415012b66e60f34590b
mayuri <pmayuri124@gmail.com>
2020-11-07 21:07:16 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Message from './Message';
 import db from './firebase';
 import firebase from 'firebase';
 import FlipMove from 'react-flip-move';
+import SendIcon from '@material-ui/icons/Send';
+import { IconButton } from '@material-ui/core';
+
 
 
 function App() {
@@ -38,14 +41,15 @@ function App() {
   return (
     <div className="App">
       <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"/>
-      <h1>hello</h1>
+      <h1>Hello</h1>
       <h2>Welcome {username}</h2>
 
       <form className="app__form">
-        <FormControl>
-        <InputLabel >Enter a message...</InputLabel>
-               <Input value={input} onChange={event => setInput(event.target.value)} />
-       <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>Send Message</Button>
+        <FormControl className="app__formControl">
+               <Input className="app__input" placeholder='Enter a message...' value={input} onChange={event => setInput(event.target.value)} />
+      <IconButton className="app__iconButton" disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}>
+       <SendIcon />
+       </IconButton>
        </FormControl>
      </form>
 
